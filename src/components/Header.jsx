5480eec1d13ce2216457dbb119d78015a843e30e
Mahadev1729 <devcampus17@gmail.com>
2025-03-17 22:37:34 +0530

    dont touch app.jsx again

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header=()=>{
+    return(
+        <>
+        <h1>Watch Box</h1>
+        <div>
+            <li><button></button></li>
+            
+        </div>
+        </>
+    );
+};
+export default Header;
