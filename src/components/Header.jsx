6d56e85004b89f0a8113910c63a65cdc6cdbb315
Mahadev1729 <devcampus17@gmail.com>
2025-03-17 23:32:57 +0530

    add the watchbox logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 const Header=()=>{
     return(
         <div>
-            
+            <img src="https://www.stottandmay.com/hubfs/Watchbox%20black%20logo.png" alt="Logo" />
         </div>
     );
 };
