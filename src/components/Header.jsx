b4c918ba9ff633659821d951e2d789491aaa4cdf
Mahadev1729 <devcampus17@gmail.com>
2025-03-18 22:49:57 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,8 @@
-const Header = () => {
+const Header=() =>{
   return (
-    <div className="absolute top-0 left-0 w-full px-8 py-4 bg-gradient-to-b from-black">
-      <img
-        className="w-44"
-        src="https://cdn.shopify.com/s/files/1/0795/5903/files/checkout_logo_2_220x.png?v=1613518474"
-        alt="Logo"
-      />
-    </div>
+    <>
+    
+    </>
   );
-};
-
+}
 export default Header;
