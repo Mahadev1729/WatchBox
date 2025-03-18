6a61d8ac270a76c55d2b3f083f331a45a672f301
Mahadev1729 <devcampus17@gmail.com>
2025-03-18 22:15:26 +0530

    set up background and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 const Header = () => {
-  return (
-    <div className="absolute top-5 left-5">
-      <img
-        className="w-40 transition-transform duration-500 hover:scale-110"
-        src="https://cdn.shopify.com/s/files/1/0795/5903/files/checkout_logo_2_220x.png?v=1613518474"
-        alt="Logo"
-      />
-    </div>
-  );
+  // return (
+  //   // <div className="absolute top-0 left-0 w-full px-8 py-4 bg-gradient-to-b from-black">
+  //   //   <img
+  //   //     className="w-44"
+  //   //     src="https://cdn.shopify.com/s/files/1/0795/5903/files/checkout_logo_2_220x.png?v=1613518474"
+  //   //     alt="Logo"
+  //   //   />
+  //   // </div>
+  // );
 };
 
 export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,13 @@
-import Header from "./Header";
-
-const Login=()=>{
-    return(
-        <div>
-            <Header/>
-            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-             alt="logo"
+const Login = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center overflow-hidden">
+            <img 
+                className="max-w-[90%] max-h-[90%] object-contain" 
+                src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+                alt="Logo"
             />
         </div>
     );
 };
+
 export default Login;
